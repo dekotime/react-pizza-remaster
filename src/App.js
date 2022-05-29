779572c902c6decc3da32b62b54b4d465329d2f8
Solitude <xynd.mail@gmail.com>
2022-05-29 21:14:29 +0300

    After 6 lesson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Header from "./components/Header";
-import Categories from "./components/Categories";
-import SortPopup from "./components/SortPopup";
-import PizzaBlock from "./components/PizzaBlock";
+import Header from './components/Header';
+import Categories from './components/Categories';
+import SortPopup from './components/SortPopup';
+import PizzaBlock from './components/PizzaBlock';
+import './scss/app.scss';
+import React from 'react';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://629376807aa3e6af1a0bc580.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => setPizzas(json));
+  }, []);
+
   return (
     <div className="wrapper">
-      <Header/>
+      <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
@@ -14,7 +24,11 @@ function App() {
             <SortPopup />
           </div>
           <h2 className="content__title">Все пиццы</h2>
-          <PizzaBlock />
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
 const Categories = () => {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  const [activeCategory, setActiveCategory] = React.useState(0)
+
+  const onSetActiveCategory = (id) => {
+    setActiveCategory(id)
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {
+          categories.map((category, index) => (
+            <li key={`${category}_${index}`}
+                onClick={() => onSetActiveCategory(index)}
+                className={activeCategory === index ? 'active' : ''}>{category}</li>))
+        }
       </ul>
     </div>
   );
