5c799335106479aa7975b8d82a05cd45eaacc48d
Solitude <xynd.mail@gmail.com>
2022-08-05 17:51:53 +0300

    Added code from 17th video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,19 @@ import LoadingBlock from '../components/PizzaBlock/LoadingBlock';
 import PizzaBlock from '../components/PizzaBlock';
 import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
-import axios from 'axios';
 import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
+import { fetchPizza } from '../redux/slices/pizzaSlice';
 
 const Home = () => {
+  const { items, status } = useSelector((state) => state.pizza);
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const isSearch = React.useRef(false);
-  const isMounted = React.useRef(false)
-
-  const [pizzas, setPizzas] = React.useState([]);
-  const [pizzasLoading, setPizzasLoading] = React.useState(true);
+  const isMounted = React.useRef(false);
 
   const { searchValue } = React.useContext(SearchContext);
 
@@ -31,22 +29,30 @@ const Home = () => {
     dispatch(setCurrentPage(number));
   };
 
-  const fetchPizzas = () => {
-    setPizzasLoading(true);
-
+  const getPizzas = async () => {
     const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sort.sortProperty.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
 
-    axios
-      .get(
-        `https://629376807aa3e6af1a0bc580.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
-      )
-      .then((res) => {
-        setPizzas(res.data);
-        setPizzasLoading(false);
-      });
+    // axios
+    //   .get(
+    //     `https://629376807aa3e6af1a0bc580.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+    //   )
+    //   .then((res) => {
+    //     setPizzas(res.data);
+    //     setPizzasLoading(false);
+    //   });
+
+    dispatch(
+      fetchPizza({
+        order,
+        sortBy,
+        category,
+        search,
+        currentPage,
+      }),
+    );
   };
 
   React.useEffect(() => {
@@ -61,7 +67,7 @@ const Home = () => {
     }
 
     isMounted.current = true;
-  }, [categoryId, sort.sortProperty, currentPage])
+  }, [categoryId, sort.sortProperty, currentPage]);
 
   React.useEffect(() => {
     if (window.location.search) {
@@ -80,18 +86,13 @@ const Home = () => {
   }, []);
 
   React.useEffect(() => {
-    window.scrollTo(0, 0);
-
-    if(!isSearch.current) {
-      fetchPizzas();
-    }
+    getPizzas();
 
     isSearch.current = false;
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
-
   const skeletons = [...new Array(6)].map((_, i) => <LoadingBlock key={i} />);
-  const itemsPizzas = pizzas
+  const itemsPizzas = items
     .filter((obj) => {
       return obj.title.toLowerCase().includes(searchValue.toLowerCase());
     })
@@ -104,7 +105,7 @@ const Home = () => {
         <SortPopup />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">{pizzasLoading ? skeletons : itemsPizzas}</div>
+      <div className="content__items">{status === 'loading' ? skeletons : itemsPizzas}</div>
       <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
