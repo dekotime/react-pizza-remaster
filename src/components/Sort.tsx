4024f33809b44796e103a8541d33261ce6f29894
Solitude <xynd.mail@gmail.com>
2022-08-11 22:14:14 +0300

    Added code from 23th video

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,77 +1,83 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {selectSort, setSortId} from '../redux/slices/filterSlice';
+import { selectSort, setSortId } from '../redux/slices/filterSlice';
 
 type SortItem = {
-  name: string;
-  sortProperty: string;
+    name: string;
+    sortProperty: string;
+};
+
+type PopupClick = MouseEvent & {
+    path: Node[];
 };
 
 export const sortList: SortItem[] = [
-  { name: 'популярности (DESC)', sortProperty: 'popular' },
-  { name: 'популярности (ASC)', sortProperty: '-popular' },
-  { name: 'цене (DESC)', sortProperty: 'price' },
-  { name: 'цене (ASC)', sortProperty: '-price' },
-  { name: 'алфавиту (DESC)', sortProperty: 'title' },
-  { name: 'алфавиту (ASC)', sortProperty: '-title' },
+    { name: 'популярности (DESC)', sortProperty: 'popular' },
+    { name: 'популярности (ASC)', sortProperty: '-popular' },
+    { name: 'цене (DESC)', sortProperty: 'price' },
+    { name: 'цене (ASC)', sortProperty: '-price' },
+    { name: 'алфавиту (DESC)', sortProperty: 'title' },
+    { name: 'алфавиту (ASC)', sortProperty: '-title' },
 ];
 
-const Sort = () => {
-  const dispatch = useDispatch();
-  const sort = useSelector(selectSort);
-  const sortRef = React.useRef<HTMLDivElement>(null);
+const Sort: React.FC = () => {
+    const dispatch = useDispatch();
+    const sort = useSelector(selectSort);
+    const sortRef = React.useRef<HTMLDivElement>(null);
 
-  const [visiblePopup, setVisiblePopup] = useState(false);
+    const [visiblePopup, setVisiblePopup] = useState(false);
 
-  const onSetActivePopup = (obj: SortItem) => {
-    dispatch(setSortId(obj));
-    setVisiblePopup(false);
-  };
+    const onSetActivePopup = (obj: SortItem) => {
+        dispatch(setSortId(obj));
+        setVisiblePopup(false);
+    };
 
-  const onClickBody = (e: any) => {
-    if (!e.path.includes(sortRef.current)) {
-      setVisiblePopup(false);
-    }
-  };
+    React.useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            const _event = event as PopupClick;
+            if (sortRef.current && !_event.path.includes(sortRef.current)) {
+                setVisiblePopup(false);
+            }
+        };
 
-  React.useEffect(() => {
-    document.body.addEventListener('click', onClickBody);
-    return () => document.body.removeEventListener('click', onClickBody);
-  }, []);
+        document.body.addEventListener('click', handleClickOutside);
 
-  return (
-    <div ref={sortRef} className="sort">
-      <div className="sort__label">
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
-        </svg>
-        <b>Сортировка по:</b>
-        <span onClick={() => setVisiblePopup(!visiblePopup)}>{sort.name}</span>
-      </div>
-      {visiblePopup && (
-        <div className="sort__popup">
-          <ul>
-            {sortList.map((obj, i) => (
-              <li
-                key={i}
-                onClick={() => onSetActivePopup(obj)}
-                className={sort.sortProperty === obj.sortProperty ? 'active' : ''}>
-                {obj.name}
-              </li>
-            ))}
-          </ul>
+        return () => document.body.removeEventListener('click', handleClickOutside);
+    }, []);
+
+    return (
+        <div ref={sortRef} className="sort">
+            <div className="sort__label">
+                <svg
+                    width="10"
+                    height="6"
+                    viewBox="0 0 10 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+                        fill="#2C2C2C"
+                    />
+                </svg>
+                <b>Сортировка по:</b>
+                <span onClick={() => setVisiblePopup(!visiblePopup)}>{sort.name}</span>
+            </div>
+            {visiblePopup && (
+                <div className="sort__popup">
+                    <ul>
+                        {sortList.map((obj, i) => (
+                            <li
+                                key={i}
+                                onClick={() => onSetActivePopup(obj)}
+                                className={sort.sortProperty === obj.sortProperty ? 'active' : ''}>
+                                {obj.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Sort;
